05156d99c4eb92b333e2a2e03a01db68f145b7de
victorhugobarbosa <victorhugobarbosa10000@gmail.com>
2024-06-12 10:35:49 -0300

    finished

--- Sistema-Solar 2/Assets/Scripts/ChangeLookAtTarget.cs
+++ Sistema-Solar 2/Assets/Scripts/ChangeLookAtTarget.cs
@@ -1,23 +1,33 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class ChangeLookAtTarget : MonoBehaviour {
 
-	public GameObject target; // the target that the camera should look at
-	private static AudioSource currentAudioSource; // Keeps track of the current playing AudioSource
+    public GameObject target; // the target that the camera should look at
+    private static AudioSource currentAudioSource;
 
-	void Start() {
-		if (target == null) 
-		{
-			target = this.gameObject;
-			Debug.Log ("ChangeLookAtTarget target not specified. Defaulting to parent GameObject");
-		}
-	}
+    void Start() {
+        if (target == null) 
+        {
+            target = this.gameObject;
+            Debug.Log("ChangeLookAtTarget target not specified. Defaulting to parent GameObject");
+        }
+    }
 
-	// Called when MouseDown on this gameObject
-	void OnMouseDown () {
-		// change the target of the LookAtTarget script to be this gameobject.
-		LookAtTarget.target = target;
-		Camera.main.fieldOfView = 60*target.transform.localScale.x;
-	}
+    // Called when MouseDown on this gameObject
+    void OnMouseDown () {
+        // change the target of the LookAtTarget script to be this gameobject.
+        LookAtTarget.target = target;
+        Camera.main.fieldOfView = 60 * target.transform.localScale.x;
+
+        if (currentAudioSource != null && currentAudioSource.isPlaying) {
+            currentAudioSource.enabled = false;
+        }
+        
+        AudioSource newAudioSource = target.GetComponent<AudioSource>();
+
+        if (newAudioSource != null) {
+            newAudioSource.enabled = true;
+            currentAudioSource = newAudioSource; 
+        }
+    }
 }
